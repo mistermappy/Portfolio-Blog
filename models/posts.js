7fd0a37abdf0f26f7706c4bd68427ae10963f225
mistermappy123 <mikhal.gelo@gmail.com>
2018-01-23 21:05:03 -0500

    3rd attempt to connect to heroku psql

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://kftcubdfwagakv:6f8a0fc821b7d7fdf3d1d0d70062666c70bc766c1d49132f5f8dd5f88847f2fe@ec2-174-129-22-84.compute-1.amazonaws.com:5432/d4c1eo62pgkt6')
+const sequelize = new Sequelize('postgres://kftcubdfwagakv:6f8a0fc821b7d7fdf3d1d0d70062666c70bc766c1d49132f5f8dd5f88847f2fe@ec2-174-129-22-84.compute-1.amazonaws.com:5432/d4c1eo62pgkt6?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory')
 
 const Post = sequelize.define('post', {
   title: {
